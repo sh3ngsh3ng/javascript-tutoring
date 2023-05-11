d3e16f6684680e8a162163156fa75d0e9644a46d
sh3ngsh3ng <yshenglim13@gmail.com>
2023-05-11 06:23:29 +0000

    added 01-intro-todom

--- dom-manipulation/01-intro-to-dom/script.js
+++ dom-manipulation/01-intro-to-dom/script.js
@@ -1,10 +1,38 @@
-
 // Sanity test to make sure javascript script is linked
 // alert('Hello!')
 
 
-let btn = document.querySelector('#my-button')
-btn.addEventListener('click', function() {
-    
-    document.querySelector('#title').style.backgroundColor = 'red'
-})
+// DOM Manipualtion
+// 1) Selecting elements (node objects)
+// 2) Applying the changes
+
+// let header = document.querySelector("#title")
+// header.innerHTML = "Introduction to DOM"
+
+// Different ways to select elements
+// 1) Select by id
+let selectByID = document.querySelector("#header")
+// 2) Select by class
+let selectByClass = document.querySelector(".header")
+// 3) Select by tag
+let selectByTag = document.querySelector("p")
+// 4) Select multiple lements with same class
+let selectMultipleClass = document.querySelectorAll(".header")
+// 5) Select multiple elements with same tag
+let selectMultipleTag = document.querySelectorAll("p")
+
+
+// Styling with DOM
+let header = document.querySelector("#title") // -> one node object
+header.style.color = 'yellow'
+header.style.backgroundColor = 'green'
+header.style.fontSize = "50px"
+// console.log(header.style)
+
+// Styling multiple elements
+let pTags = document.querySelectorAll("p") // -> array of node object
+// console.log(pTags)
+for (let x of pTags) {
+    x.style.backgroundColor = 'red'
+    x.style.fontSize = '30px'
+}
